d967410dee6ed72898fcddf0184c6e90ae384aa2
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2019-11-15 18:50:37 +0800

    Checking internet connection

--- app/isnetav.m
+++ app/isnetav.m
@@ -1,6 +1,6 @@
 function x=isnetav
 disp('-->> Checking status internet connection');
-[~,b]=dos('ping -n 1 www.google.com');
+[~,b]=dos('ping -n 1 www.github.io');
 n=strfind(b,'Lost');
 n1=b(n+7);
 if(n1=='0')
