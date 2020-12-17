9d44a524c608cd79aacd9178ab5c8a731806ea41
Ariosky <ariosky.areces@neuroinformatics-collaboratory.org>
2020-12-17 20:10:51 +0800

    Importing PLG EEG format and marks

--- functions/eeglab_preproc.m
+++ functions/eeglab_preproc.m
@@ -17,7 +17,8 @@ function EEG  = eeglab_preproc(subID, file_name, data_type, eeglab_path, varargi
 %   max_freq    - Integer maximun frequency to filtering the data (key='max_freq', value=from 1 to 92)
 %   save_path   - full path to save the cleanned EEG (key='save_path', value="fullpath")
 %   freq_list   - vector of frequencies point to show in plots (key='freq_list', value=[1 6 10 18])
-%   labels      - lisat of labels to select in the data (key='labels', value={'L1';'L2';'L3';.......;'Ln'})
+%   labels      - list of labels to select in the data (key='labels', value={'L1';'L2';'L3';.......;'Ln'})
+%   read_marks  - true or false if you want to read the good time segments and reject the bad time segments
 %   notime      - [min max] in seconds. Epoch latency or continuous dataset time range
 %                 to exclude from the new dataset. For continuous data, may be
 %                 [min1 max1; min2 max2; ...] to exclude several time ranges. For epoched
@@ -26,6 +27,11 @@ function EEG  = eeglab_preproc(subID, file_name, data_type, eeglab_path, varargi
 %
 % Author: Eduardo Gonzalez-Moreira
 % Date: Oct-2020
+%
+%
+% Updates by:   Ariosky Areces Gonzalez
+%               Deirel Paz Linares
+%
 
 %%
 %% Step 1: Preparing workspace.
@@ -54,30 +60,41 @@ end
 addpath(eeglab_path);
 eeglab nogui;
 
-
 %% Step2: Import data.
-switch data_type
+switch lower(data_type)
     case 'set'
-        EEG = pop_loadset(file_name);
+        EEG         = pop_loadset(file_name);
     case 'mat'
         load(file_name);
+        srate       = SAMPLING_FREQ;
         % For Pedrito's data selection
         load('templates/EEG_template.mat');
-        EEG.xmax = max(data(:));
-        EEG.data = data;
-        EEG.nbchan = size(data,1);
-        EEG.pnts = size(data,2);
-        EEG.times(size(data,2)+1:end) = [];
-        
+        EEG.srate   = srate;
+        EEG.age     = age;
+        EEG.data    = data;
+        EEG.nbchan  = size(data,1);
+        EEG.pnts    = size(data,2); 
+        EEG.xmin    = 0;
+        EEG.xmax    = EEG.xmin+(EEG.pnts-1)*(1/EEG.srate);       
+        EEG.times   = (0:EEG.pnts-1)/EEG.srate.*1000;
         if(exist('labels','var'))
-            EEG.chanlocs(length(labels)+1:end,:) = [];
-            new_labels = labels;
-            [EEG.chanlocs.labels] = new_labels{:};
+            EEG.chanlocs(length(labels)+1:end,:)    = [];
+            new_labels                              = labels;
+            [EEG.chanlocs.labels]                   = new_labels{:};
         end
     case 'dat'
-        EEG = pop_loadBCI2000(file_name);
-    case 'PLG'
-        EEG = pop_load_plg(fullfile(rawDataFiles.folder,file_name));
+        EEG         = pop_loadBCI2000(file_name);
+    case 'plg'
+        EEG         = readplot_plg(fullfile(file_name));
+        template    = load('templates/EEG_template.mat');
+        chanlocs    = template.EEG.chanlocs;        
+        load('templates/labels_nomenclature.mat');
+        orig_labels = labels_match(:,1);      
+        for i=1:length(orig_labels)
+            chanlocs(i).labels = labels_match{find(strcmp({EEG.chanlocs.labels},num2str(orig_labels{i})),1),2};
+        end 
+        EEG.chanlocs = chanlocs;
+        EEG.chaninfo = template.EEG.chaninfo;
     case 'edf'
         EEG = pop_biosig(file_name);
         % For cuban dataset
@@ -86,12 +103,13 @@ switch data_type
         new_labels = replace({EEG.chanlocs.labels}',' ','');
         [EEG.chanlocs.labels] = new_labels{:};
 end
-EEG.subID = subID;
+EEG.setname     = subID;
+EEG.subID       = subID;
 
 %% Filtering by user labels
 if(exist('labels','var'))
     disp ("-->> Cleanning EEG bad Channels by user labels");
-    EEG  = remove_eeg_channels_by_labels(new_labels,EEG);
+    EEG  = remove_eeg_channels_by_labels(labels,EEG);
 end
 %% Step 3: Visualization.
 if verbosity
@@ -103,13 +121,13 @@ if EEG.srate > 300
     EEG = pop_resample(EEG, 200);
 end
 
-%% Step 5: Filtering the data at 0Hz and 45 Hz.
+%% Step 5: Filtering the data at 0Hz and Max frequency Hz.
 EEG = pop_eegfiltnew(EEG, 'locutoff', 0, 'hicutoff',max_freq, 'filtorder', 3300);
 
-% %% Step 6: Import channel info.
+%% Step 6: Import channel info.
 EEG = pop_chanedit(EEG, 'lookup',fullfile(eeglab_path,'plugins/dipfit3.4/standard_BEM/elec/standard_1005.elc'),'eval','chans = pop_chancenter( chans, [],[]);');
 clear_ind = [];
-for i=1:length(new_labels)
+for i=1:length(EEG.chanlocs)
     if(isempty(EEG.chanlocs(i).X))
         clear_ind = [clear_ind; i];
     end
@@ -117,6 +135,34 @@ end
 EEG.chanlocs(clear_ind) = [];
 EEG.data(clear_ind,:) = [];
 EEG.nbchan = length(EEG.chanlocs);
+
+if(exist('read_marks','var'))
+    if(isfield(EEG,'TW') && ~isempty(EEG.TW))
+        regions = [EEG.TW];
+        rej_regions = [];
+        for i=1:length(regions)
+            if(isempty(rej_regions))
+                time_end = regions(i).start - 1;
+                rej_regions = [0 time_end];
+            else
+                time_start = regions(i-1).end + 1;
+                time_end = regions(i).start - 1 ;
+                rej_regions = [rej_regions; time_start time_end];
+            end
+        end
+        time_start = regions(end).end + 1;
+        time_end = EEG.times(end) ;
+        rej_regions = [rej_regions; time_start time_end];
+        
+        EEG = eeg_eegrej(EEG, rej_regions);
+        EEG.RejTime = rej_regions;
+    end
+    if(exist('notime','var'))
+        EEG = eeg_eegrej( EEG, notime);
+        EEG.RejTime = notime;
+    end
+end
+
 if verbosity
     figure;
     [spectra,freqs] = spectopo(EEG.data,0,EEG.srate,'limits',[0 max_freq NaN NaN -10 10],'chanlocs',EEG.chanlocs,'chaninfo',EEG.chaninfo,'freq',freq_list);
--- functions/import_eeg_format.m
+++ functions/import_eeg_format.m
@@ -1,4 +1,5 @@
 function EEG = import_eeg_format(subject_info, selected_data_set, base_path)
+
 data_type    = selected_data_set.preprocessed_data.format;
 if(~isequal(selected_data_set.preprocessed_data.labels_file_path,"none"))
     user_labels = jsondecode(fileread(selected_data_set.preprocessed_data.labels_file_path));    
@@ -7,38 +8,41 @@ if(selected_data_set.preprocessed_data.clean_data.run)
     if(isequal(lower(selected_data_set.preprocessed_data.clean_data.toolbox),'eeglab'))
         toolbox_path = selected_data_set.preprocessed_data.clean_data.toolbox_path;
         max_freq     = selected_data_set.preprocessed_data.clean_data.max_freq;
+        read_marks   = selected_data_set.preprocessed_data.clean_data.read_marks;
         %         save_path    = fullfile(selected_data_set.report_output_path,'Reports',selected_data_set.protocol_name,subject_info.name,'EEGLab_preproc');
         if(exist('user_labels','var'))
-            EEG          = eeglab_preproc(subject_info.name, base_path, data_type, toolbox_path, 'verbosity', false, 'max_freq', max_freq, 'labels', user_labels);
+            EEG      = eeglab_preproc(subject_info.name, base_path, data_type, toolbox_path, 'verbosity', true, 'max_freq', max_freq, 'labels', user_labels, 'read_marks', read_marks);
         else
-            EEG          = eeglab_preproc(subject_info.name, base_path, data_type, toolbox_path, 'verbosity', false, 'max_freq', max_freq);
+            EEG      = eeglab_preproc(subject_info.name, base_path, data_type, toolbox_path, 'verbosity', true, 'max_freq', max_freq, 'read_marks', read_marks);
         end
         EEG.labels   = {EEG.chanlocs(:).labels};
     end
 else
-    EEG = struct;
-    EEG.subID = subject_info.name;
+    EEG         = struct;
+    EEG.subID   = subject_info.name;
+    EEG.setname = subject_info.name;
     switch data_type
         case 'edf'
-            [hdr, data]= edfread(base_path);
+            [hdr, data]     = edfread(base_path);
         case 'plg'
             [pat_info, inf_info, plg_info, mrk_info, win_info, cdc_info, states_name] = plg2matlab(base_path);
             % creating output structure
-            data = plg_info.data;            
-            hdr.pat_info = pat_info;
-            hdr.inf_info = inf_info;
-            hdr.mrk_info = mrk_info;
-            hdr.win_info = win_info;
-            hdr.cdc_info = cdc_info;
+            data            = plg_info.data;            
+            hdr.pat_info    = pat_info;
+            hdr.inf_info    = inf_info;
+            hdr.mrk_info    = mrk_info;
+            hdr.win_info    = win_info;
+            hdr.cdc_info    = cdc_info;
             hdr.states_name = states_name;
-            hdr.label = inf_info.PLGMontage;
+            hdr.label       = inf_info.PLGMontage;
     end
-    EEG.data = data;
-    EEG.labels = strrep(hdr.label,'REF','');
-    EEG.srate = hdr.samples(1);    
+    EEG.data    = data;
+    EEG.labels  = strrep(hdr.label,'REF','');
+    EEG.srate   = hdr.samples(1);    
     if(exist('user_labels','var'))
         disp ("-->> Cleanning EEG bad Channels by user labels");
-        EEG  = remove_eeg_channels_by_labels(user_labels,EEG);
+        EEG         = remove_eeg_channels_by_labels(user_labels,EEG);
+        EEG.labels  = {EEG.chanlocs(:).labels};
     end
 end
 end
