b9b1244a6efec7d4dd61d801e07adb88ae69febd
Ariosky Areces Gonzalez <ariosky@neuroinformatics-collaboratory.org>
2022-03-08 13:29:51 +0800

    Updating structure

--- functions/clean_data/import_eeg_format.m
+++ functions/clean_data/import_eeg_format.m
@@ -32,6 +32,7 @@ if(properties.clean_data.run)
         chan_action     = properties.clean_data.rej_or_interp_chan.action;
         select_events   = properties.clean_data.select_events;
         clean_art_params = properties.clean_data.clean_artifacts;
+        decompose_ica   = properties.clean_data.decompose_ica;
         if(isequal(properties.name,'raw_data'))
            use_raw_data = true; 
         else
@@ -45,7 +46,7 @@ if(properties.clean_data.run)
         end        
         EEGs      = eeglab_preproc(subID, data_path, data_type, toolbox_path, 'verbosity', true, 'max_freq', max_freq,...
             'labels', user_labels, 'select_events', select_events, 'use_raw_data', use_raw_data, 'derivatives', derivatives,...
-            'save_path', subject_report_path, 'chan_action', chan_action, 'clean_art_params', clean_art_params);        
+            'save_path', subject_report_path, 'chan_action', chan_action, 'clean_art_params', clean_art_params, 'decompose_ica', decompose_ica);        
         for i=1:length(EEGs)
             EEGs(i).labels   = {EEGs(i).chanlocs(:).labels};
         end
