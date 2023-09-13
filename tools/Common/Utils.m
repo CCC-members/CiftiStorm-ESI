07f16cebcd0c5d5259de5afdc66b0ac1d5032245
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2023-09-13 12:26:48 +0800

    Delete Utils.m

--- tools/Common/Utils.m
+++
@@ -1,7 +0,0 @@
-
-
-% Ploting normals
-% 
-% quiver3(Rpial(:,1),Rpial(:,2),Rpial(:,3),Npial(:,1),Npial(:,2),Npial(:,3),'Color','b','AutoScaleFactor',3);
-% 
-% quiver3(Rwhite(:,1),Rwhite(:,2),Rwhite(:,3),Nwhite(:,1),Nwhite(:,2),Nwhite(:,3),'Color','r','AutoScaleFactor',3);
