7d9002e56af6b2a5c793bafbea8163e989db6a70
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2023-09-13 20:42:41 +0800

    Check spelling

--- tools/BC-VARETA/dataConverter/export/convertToBC_V_Structure.m
+++ tools/BC-VARETA/dataConverter/export/convertToBC_V_Structure.m
@@ -1,22 +1,22 @@
 function  convertToBC_V_Structure(data,BC_V_Workspace)
 for i=1:length(data)
-    dataS                       = data(i);
+    dataSt                       = data(i);
     base_path                   = fullfile(BC_V_Workspace);
-    modality                    = dataS.Modality;
-    subID                       = dataS.Name;
+    modality                    = dataSt.Modality;
+    subID                       = dataSt.Name;
     disp(strcat("-->> Exporting subject:" , subID));
-    HeadModels.HeadModel        = load(dataS.Structural.HeadModel);
-    Cdata                       = load(dataS.Structural.Channel);
-    Shead                       = load(dataS.Structural.Head);
-    Sout                        = load(dataS.Structural.Outerskull);
-    Sinn                        = load(dataS.Structural.Innerskull);
-    Scortex.Sc                  = load(dataS.Structural.Cortex);
+    HeadModels.HeadModel        = load(dataSt.Structural.HeadModel);
+    Cdata                       = load(dataSt.Structural.Channel);
+    Shead                       = load(dataSt.Structural.Head);
+    Sout                        = load(dataSt.Structural.Outerskull);
+    Sinn                        = load(dataSt.Structural.Innerskull);
+    Scortex.Sc                  = load(dataSt.Structural.Cortex);
     Scortex.iCortex             = 1;
     if(isequal(Scortex.Sc.Atlas(Scortex.Sc.iAtlas).Name,'Structures') || isempty(Scortex.Sc.Atlas(Scortex.Sc.iAtlas).Scouts))
         Scortex.Sc.Atlas.Name      = 'User scouts';
         Scortex.Sc.Atlas.Scouts    = generate_scouts(Scortex.Sc);
     end
-    MEEG = load(dataS.Functional);
+    MEEG = load(dataSt.Functional);
     action                      = 'new';
     if(isequal(modality,'EEG'))
         labels                  = {MEEG.chanlocs(:).labels};
