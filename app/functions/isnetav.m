1c1f9942fec519c43bbe1a990ca8f812bc8456fb
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2024-01-25 20:40:00 +0800

    Updating structural process

--- app/functions/isnetav.m
+++ app/functions/isnetav.m
@@ -1,13 +1,25 @@
 function x=isnetav
-disp('-->> Checking status internet connection');
-[~,b]=dos('ping -n 1 www.github.io');
-n=strfind(b,'Lost');
-n1=b(n+7);
-if(n1=='0')
+disp('-->> Checking internet status');
+[~,b]=system('ping -c 5 -W 15 www.github.com');
+n=strfind(b,'received');
+if ismac
+    % Code to run on Mac platform
+elseif isunix
+   n1=b(n-2);
+elseif ispc
+   n1=b(n+7);
+else
+    disp('Platform not supported');
+end
+if(n1=='5')
     x=1;
-    disp('-->> The internet connection is good');
+    disp('-->> Internet connection Ok');
 else
     x=0;
-    disp('-->> There some problems with the insternet connection');
+    cprintf('red','-->> We could not get you online'); fprintf('\n');
+    cprintf('red','-->> There migth be either a problem with the internet connection or matlab proxy configuration'); fprintf('\n');
+    cprintf('red','-->> We could not get the BC-VARETA latest version right now.');fprintf('\n');
+    cprintf('red','-->> Please check your internet configuration and try to update again.');fprintf('\n');
+    disp('=================================================================');
 end
 end
