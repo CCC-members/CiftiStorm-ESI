c54a33722d16ddd46ee62f14c1802f963747ffde
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2024-10-09 16:58:42 +0800

    Update cfs_get.m

--- cfs_properties/functions/cfs_get.m
+++ cfs_properties/functions/cfs_get.m
@@ -9,7 +9,7 @@ end
 % Get required context structure
 switch contextName
     case 'cfs_dir'
-        output1 = fullfile(getUserDir(),'.CiftiStorm');
+        output1 = fullfile(getUserDir(),'.CiftiStorm');    
     case 'defaults_dir'
         cfs_db_dir = fullfile(getUserDir(),'.CiftiStorm');
         output1 =  fullfile(cfs_db_dir,'defaults','anatomy');
@@ -23,6 +23,8 @@ switch contextName
         else
             output1 = [];
         end
+    case 'datasets_file'
+        output1 = fullfile(cfs_get( 'cfs_dir' ),'Datasets.json');
     case 'bst_default_eeg'
         output1 = 'https://github.com/brainstorm-tools/brainstorm3/raw/master/defaults/eeg';
     case 'openmeeg'
